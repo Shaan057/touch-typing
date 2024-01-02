92170678ac4e108237854502f0597317b6eb4fac
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-02 18:49:56 +0530

    minor styling

--- src/components/typing/index.js
+++ src/components/typing/index.js
@@ -1,5 +1,5 @@
 import './index.css'
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 
 const wordsTypeObj = [
     { type: 'bigrams', id: 2 },
@@ -7,17 +7,16 @@ const wordsTypeObj = [
     { type: 'tetragrams', id: 4 }
 ]
 
-
 const Typer = () => {
-    const keysArr = ['!', 'a', 's', 'd', 'f', 'j', 'Control', 'Alt', ' ', 'k', 'l', 'Enter']
-    const [words, setWords] = useState('asdfjkl')
+    const keysArr = ['1', 'a', 's', 'd', 'f', 'j', 'Control', 'Alt', ' ', '0', 'k', 'l', ';', 'Enter']
+    const [words, setWords] = useState('asdfjkl;')
     const [input, setInput] = useState('')
     const [isInputCorrect, setIsInputCorrect] = useState(false)
     const [wordsType, setWordsType] = useState(wordsTypeObj[0].type)
     const [practiceWords, setPracticeWords] = useState('as sa as sa')
     const [start, setStart] = useState(0)
     const [count, setCount] = useState(2)
-    const [totalSeconds, setTotalSeconds] = useState(300)
+    const [totalSeconds, setTotalSeconds] = useState(20)
     const [isTimerStarted, startTimer] = useState(false)
     const [totalWords, setTotalWords] = useState(0)
     const [correctWords, setCorrectWords] = useState(0)
@@ -25,9 +24,34 @@ const Typer = () => {
     const [session, setSession] = useState(1)
     const [uniqueId, setUniqueId] = useState(null)
     const [inputKey, setInputKey] = useState(null)
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        if (isTimerStarted) { inputRef.current.focus() }
+    })
 
     if (isTimerStarted && totalSeconds === 0) {
         clearInterval(uniqueId)
+        startTimer(false)
+    }
+
+    const resetTimer = () => {
+        setIsInputCorrect(false)
+        setInput('')
+        setTotalSeconds(20)
+        setWordsType(wordsTypeObj[0].type)
+        setPracticeWords('as sa as sa')
+        setCount(0)
+        setTotalWords(0)
+        setCorrectWords(0)
+        setKeysTyped(0)
+        setSession(1)
+        setUniqueId(null)
+        setInputKey(null)
+    }
+
+    if (totalSeconds === 0) {
+        resetTimer()
     }
 
     let startCountDown = useCallback(() => {
@@ -97,28 +121,41 @@ const Typer = () => {
         }
     }
 
+    const onClickStartButton = () => {
+        startTimer(true)
+        startCountDown()
+    }
+
+    const onClick9Button = () => {
+        setIsInputCorrect(true)
+        randommize()
+        setSession((prev) => prev + 1)
+        setInput('')
+    }
     const onChangeInput = (event) => {
-        if (isTimerStarted === false) {
-            startTimer(true)
-            startCountDown()
-        }
-        setInput(event.target.value)
+        const { value } = event.target
+        setInput(value)
+        setInput(value)
         setKeysTyped((prev) => prev + 1)
         setTotalWords((prev) => prev + 1)
-
+        if (value === '0') {
+            resetTimer()
+        }
+        if (value === '9') {
+            onClick9Button()
+        }
     }
 
     const onKeyDownEventTriggered = (event) => {
-        if (event.key === '!') {
+        if (event.key === '1') {
             setInput('')
         }
         setInputKey(event.key)
     }
 
     const onKeyUpEventTriggered = (event) => {
-        console.log(event.key)
         const { key } = event
-        if (key === '!') {
+        if (key === '1') {
             setInput('')
         }
         checkAccuracy(event.key)
@@ -127,9 +164,9 @@ const Typer = () => {
             setInput('')
             setIsInputCorrect(true)
             randommize()
-
         }
     }
+
 
 
     const minutes = Math.floor(totalSeconds / 60)
@@ -158,15 +195,17 @@ const Typer = () => {
             </div>
             <div className='input-container'>
                 <p className='words'>{practiceWords}</p>
-                <input
+                {isTimerStarted ? <input
+                    ref={inputRef}
                     className='input'
-                    placeholder='press left ! key to clear'
+                    placeholder={isTimerStarted ? 'press 1 to clear, 0 to reset' : 'Start Typing'}
                     type='text'
                     value={input}
+                    onChange={onChangeInput}
                     onKeyDown={onKeyDownEventTriggered}
                     onKeyUp={onKeyUpEventTriggered}
-                    onChange={onChangeInput}
-                />
+                /> : <button className='button' type='button' onClick={onClickStartButton}>Start
+                </button>}
                 <div className='result-container'>
                     <h1 className='result-heading'>WPM: {wordsPerMinute}</h1>
                     <h1 className='result-heading'>Accuracy: {accuracy || 0}%</h1>
